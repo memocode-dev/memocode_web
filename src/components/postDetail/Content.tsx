60cd343629c93f97cec78e4502e8e7ea19bae2f8
audgks5551 <audgsk5551@daum.net>
2024-03-19 18:08:17 +0900

    Fix: post detail에서 markdown css가 적용될 수 있도록 수정

--- src/components/postDetail/Content.tsx
+++ src/components/postDetail/Content.tsx
@@ -5,7 +5,7 @@ const Content = ({post}: { post: PostDetailDTO }) => {
     return (
         <div className="bg-white dark:bg-[#1E1E1E] border-b border-b-gray-300 px-5 py-10">
             <div className="text-lg font-medium leading-snug break-all">
-                <div dangerouslySetInnerHTML={{__html: MarkdownView.render(post?.content || "")}}></div>
+                <div className="markdown-body" dangerouslySetInnerHTML={{__html: MarkdownView.render(post?.content || "")}}></div>
             </div>
         </div>
     )
