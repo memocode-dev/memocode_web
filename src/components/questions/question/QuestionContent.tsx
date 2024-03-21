224d876ba0ddbd5f0f6698670906f5c11286ddcd
miruy <94223746+miruy@users.noreply.github.com>
2024-03-21 18:15:19 +0900

    Fix : Q&A 상세페이지 UI 1차구현 (#24)

--- src/components/questions/question/QuestionContent.tsx
+++ src/components/questions/question/QuestionContent.tsx
@@ -44,7 +44,6 @@ const QuestionContent = () => {
             </div>
 
             <div className="flex justify-center space-x-2">
-
                 <div onClick={handleLike} className="cursor-pointer">
                     {!like && <AiOutlineLike className="animate-bounce text-gray-500 dark:text-gray-400 w-9 h-9"/>}
                     {like && <AiFillLike className="text-indigo-500 w-9 h-9"/>}
@@ -52,6 +51,7 @@ const QuestionContent = () => {
 
                 <div className="flex justify-center items-center bg-gray-200 dark:bg-neutral-700 w-9 h-9 rounded-full">
                     <span>{count}</span>
+
                 </div>
             </div>
         </div>
