ba107f3367bd1dbe0c30037f7063c09dde2bc185
audgks5551 <audgsk5551@daum.net>
2024-03-21 21:55:17 +0900

    [Bug/Enhancement] 회원가입 페이지에서 Form이 TopBar에 가려지는 현상 제거 && 회원가입 페이지에서 아이디와 닉네임 유효성 검사 구현

--- src/pages/Anonymous.tsx
+++ src/pages/Anonymous.tsx
@@ -7,6 +7,7 @@ import {Button} from "@/components/ui/button";
 import {Label} from "@radix-ui/react-menubar";
 import {Input} from "@/components/ui/input.tsx";
 import {parseJwt} from "@/lib/jwt.ts";
+import {ErrorResponse} from "@/vite-env";
 
 type Inputs = {
     username: string
@@ -22,6 +23,7 @@ const Anonymous = () => {
     const {
         register,
         handleSubmit,
+        formState: {errors},
     } = useForm<Inputs>()
 
     const { mutate } = useCreateUser({
@@ -53,7 +55,13 @@ const Anonymous = () => {
                 }, 1000);
             },
             onError: (error) => {
-                console.log(error)
+                console.log(error);
+
+                if (error.response?.data) {
+                    const data = error.response?.data as ErrorResponse;
+                    toast.error(data.message);
+                }
+
                 toast.error("관리자에게 문의하세요.");
             }
         }
@@ -74,15 +82,59 @@ const Anonymous = () => {
     }
 
     return (
-        <div className="flex flex-1">
-            <form className="flex-1 max-w-xl mx-auto space-y-4 mt-6" onSubmit={handleSubmit(onSubmit)}>
-                <Label>아이디</Label>
-                <Input {...register("username", { required: true })} />
+        <div className="flex flex-1 justify-center items-center">
+            <form className="flex-1 max-w-xl mx-auto space-y-12 mt-6" onSubmit={handleSubmit(onSubmit, () => {
+                toast.error("필수 입력 필드를 확인해주세요.");
+            })}>
+                <div className="relative">
+                    <Label className="mb-1">
+                        <span>아이디</span>
+                        <span className="text-red-400">*</span>
+                    </Label>
+                    <Input {...register("username", {
+                        required: "아이디를 입력해주세요.",
+                        pattern: {
+                            value: /^[A-Za-z0-9]+$/,
+                            message: "아이디는 영어와 숫자만 사용할 수 있습니다."
+                        },
+                        minLength: {
+                            value: 4,
+                            message: "아이디는 최소 4자 이상이어야 합니다.",
+                        },
+                        maxLength: {
+                            value: 20,
+                            message: "아이디는 최대 20자까지 가능합니다.",
+                        },
+                    })} />
+                    <div className="absolute text-sm left-2 -bottom-6 text-red-500">{errors.username && errors.username.message}</div>
+                </div>
 
-                <Label>닉네임</Label>
-                <Input {...register("nickname", { required: true })} />
 
-                <Button type="submit">회원가입</Button>
+                <div className="relative">
+                    <Label className="mb-1">
+                        <span>닉네임</span>
+                        <span className="text-red-400">*</span>
+                    </Label>
+                    <Input {...register("nickname", {
+                        required: "닉네임을 입력해주세요.",
+                        pattern: {
+                            value: /^[A-Z0-9가-힣]+$/,
+                            message: "닉네임은 영대문자, 숫자, 한국어만 사용할 수 있습니다."
+                        },
+                        minLength: {
+                            value: 4,
+                            message: "닉네임은 최소 4자 이상이어야 합니다.",
+                        },
+                        maxLength: {
+                            value: 20,
+                            message: "닉네임은 최대 20자까지 가능합니다.",
+                        },
+                    })} />
+                    <div className="absolute text-sm left-2 -bottom-6 text-red-500">{errors.nickname && errors.nickname.message}</div>
+                </div>
+
+
+                <Button type="submit" className="bg-indigo-500 dark:bg-indigo-700 dark:text-white">회원가입</Button>
             </form>
         </div>
     )
