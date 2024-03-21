c8d2d78559edb12f82acf4f76284e880de2decec
miruy <94223746+miruy@users.noreply.github.com>
2024-03-21 18:15:19 +0900

    Fix : Q&A 상세페이지 UI 1차구현 (#24)

---
+++ src/components/questions/question/QuestionTitle.tsx
@@ -0,0 +1,28 @@
+import {faker} from "@faker-js/faker";
+
+const QuestionTitle = () => {
+
+    // 가짜 데이터 생성
+    const fakerData = {
+        title: faker.lorem.sentence(),
+        author: faker.person.fullName(),
+    };
+
+    return (
+        <div className="bg-white dark:bg-[#1E1E1E] p-5">
+            <div className="text-2xl font-bold leading-snug break-all">
+                {fakerData.title}
+            </div>
+
+            <div className="flex justify-between items-center border-b border-b-gray-300 pt-5 pb-2">
+                <div className="tracking-wider">{fakerData.author}</div>
+
+                <div className="text-gray-500 dark:text-gray-300 tracking-wider">
+                    2024.03.11
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default QuestionTitle
