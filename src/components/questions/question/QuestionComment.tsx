c512b861625ff6fd5cbf677ed860e8e3bd05abb6
miruy <dbflarla4966@naver.com>
2024-03-22 22:24:24 +0900

    [Enhancement] 답변등록 폼에 모나코에디터 추가 및 커스텀

--- src/components/questions/question/QuestionComment.tsx
+++ src/components/questions/question/QuestionComment.tsx
@@ -1,14 +1,60 @@
+import {useParams} from "react-router-dom";
+import {useContext, useEffect, useRef, useState} from "react";
+import {ThemeContext} from "@/context/ThemeContext.tsx";
+import {useForm} from "react-hook-form";
+import QuestionCommentMonacoEditor from "@/components/questions/question/QuestionCommentMonacoEditor.tsx";
+
 const QuestionComment = () => {
+
+    const {questionId} = useParams()
+    const {theme} = useContext(ThemeContext);
+    const {setValue, watch} = useForm({
+        defaultValues: {
+            answer: ""
+        }
+    })
+    const divRef = useRef<HTMLDivElement | null>(null);
+    const [width, setWidth] = useState<number>(0);
+
+    useEffect(() => {
+        const div = divRef.current;
+        if (div) {
+            // ResizeObserver 인스턴스 생성
+            const resizeObserver = new ResizeObserver(entries => {
+                const { width} = entries[0].contentRect;
+                setWidth(width - 5);
+            });
+
+            // 관찰 시작
+            resizeObserver.observe(div);
+
+            // 컴포넌트가 언마운트 될 때 관찰 중단
+            return () => resizeObserver.unobserve(div);
+        }
+    }, []);
+
     return (
         <div className="flex flex-1 bg-white dark:bg-[#1E1E1E]">
             <div className="flex-1 py-10 px-5">
                 <div className="mb-1 font-semibold text-gray-700 dark:text-gray-300">답변하기</div>
-                <div className="flex flex-1 space-x-2">
-                    <textarea
-                        placeholder="에디터로 변경예정"
-                        className="flex-1 resize-none border border-gray-200 bg-white dark:bg-[#1E1E1E] outline-none rounded-lg h-44 p-2"></textarea>
+
+                <div ref={divRef} className="flex flex-1">
+                    <div className="flex w-full h-[490px]">
+                        <QuestionCommentMonacoEditor
+                            key={questionId}
+                            width={`${width}%`}
+                            height={`${100}%`}
+                            language="markdown"
+                            theme={theme === "light" ? "vs" : "vs-dark"}
+                            onChange={(value) => setValue("answer", value)}
+                            value={watch("answer")}
+                        />
+                    </div>
+                </div>
+
+                <div className="flex flex-1 justify-end">
                     <div
-                        className="flex w-24 h-44 bg-indigo-400 hover:bg-indigo-500 dark:bg-indigo-700 dark:hover:bg-indigo-800 text-white rounded-lg p-2 justify-center items-center">
+                        className="flex w-16 h-12 bg-indigo-400 hover:bg-indigo-500 dark:bg-indigo-700 dark:hover:bg-indigo-800 text-white rounded-lg p-2 justify-center items-center mt-2">
                         <div>등록</div>
                     </div>
                 </div>
---
+++ src/components/questions/question/QuestionCommentMonacoEditor.tsx
@@ -0,0 +1,189 @@
+import {useEffect, useRef} from "react";
+import * as monaco from 'monaco-editor';
+import {Toggle} from "@/components/ui/toggle.tsx";
+import {
+    LuBold,
+    LuCode2,
+    LuHeading1,
+    LuHeading2,
+    LuHeading3,
+    LuItalic,
+    LuLink,
+    LuPalette,
+    LuStrikethrough
+} from "react-icons/lu";
+import {FaListOl, FaListUl} from "react-icons/fa";
+
+interface QuestionCommentMonacoEditorProps {
+    language: string;
+    theme: "vs" | "vs-dark";
+    value?: string;
+    onChange?: (value: string) => void;
+    width: string;
+    height: string;
+}
+
+const QuestionCommentMonacoEditor = ({
+                                         language,
+                                         theme,
+                                         onChange,
+                                         value,
+                                         width,
+                                         height
+                                     }: QuestionCommentMonacoEditorProps) => {
+    const editorRef = useRef<HTMLDivElement>(null);
+    const monacoInstanceRef = useRef<monaco.editor.IStandaloneCodeEditor | null>(null);
+
+
+    // 볼드체 버튼
+    const setBold = () => {
+        const editor = monacoInstanceRef.current;
+        if (!editor) return;
+
+        const selection = editor.getSelection(); // 모나코에디터에서 사용자가 선택한 영역
+
+        if (!selection) return;
+
+        const text = editor?.getModel()?.getValueInRange(selection); // 선택된 영역을 모나코에디터의 IRange타입으로 맞춤
+        const boldedText = `**${text}**`;
+
+        const operation: monaco.editor.IIdentifiedSingleEditOperation = { // bold 기능을 에디터에 정의
+            range: selection, // 이제 'IRange' 타입과 호환됩니다.
+            text: boldedText,
+            forceMoveMarkers: true,
+        };
+
+        editor.executeEdits("my-source", [operation]); // 실행
+    };
+
+    useEffect(() => {
+        if (editorRef.current) {
+            monacoInstanceRef.current = monaco.editor.create(editorRef.current, {
+                value: value,
+                language: language,
+                minimap: {enabled: false},
+                automaticLayout: true,
+                theme: theme,
+                mouseWheelZoom: true,
+                fontSize: 20,
+                lineNumbers: "off",
+                scrollbar: {horizontal: "hidden"},
+                scrollBeyondLastLine: false,
+                wordWrap: 'on',
+                padding: {top: 10, bottom: 10},
+                suggestOnTriggerCharacters: false,
+                renderLineHighlight: "none"
+            });
+
+            // onChange event
+            const disposable = monacoInstanceRef.current.onDidChangeModelContent(() => {
+                const newValue = monacoInstanceRef.current?.getValue();
+                if (onChange) {
+                    onChange(newValue || '');
+                }
+            });
+
+            return () => {
+                disposable.dispose();
+                monacoInstanceRef.current?.dispose();
+            };
+        }
+    }, [theme]);
+
+    useEffect(() => {
+        const editor = monacoInstanceRef.current;
+        if (editor && value !== undefined && value !== editor.getValue()) {
+            editor.setValue(value);
+        }
+    }, [value]);
+
+    return (
+        <>
+            <div className="flex relative space-y-10">
+                <div className="flex flex-col absolute z-10 top-1 left-1 space-y-1">
+                    <Toggle
+                        aria-label="Toggle bold"
+                        className="flex hover:bg-gray-100 data-[state=on]:bg-gray-100 dark:hover:bg-neutral-700 dark:data-[state=on]:bg-black hover:text-neutral-400 ring-0"
+                        onClick={setBold}
+                    >
+                        <LuBold className="h-5 w-5"/>
+                    </Toggle>
+
+                    <Toggle
+                        aria-label="Toggle italic"
+                        className="flex hover:bg-gray-100 data-[state=on]:bg-gray-100 dark:hover:bg-neutral-700 dark:data-[state=on]:bg-black hover:text-neutral-400 ring-0"
+                    >
+                        <LuItalic className="h-5 w-5"/>
+                    </Toggle>
+
+                    <Toggle
+                        aria-label="Toggle strikethrough"
+                        className="flex hover:bg-gray-100 data-[state=on]:bg-gray-100 dark:hover:bg-neutral-700 dark:data-[state=on]:bg-black hover:text-neutral-400 ring-0"
+                    >
+                        <LuStrikethrough className="h-5 w-5"/>
+                    </Toggle>
+
+                    <Toggle
+                        aria-label="Toggle code"
+                        className="flex hover:bg-gray-100 data-[state=on]:bg-gray-100 dark:hover:bg-neutral-700 dark:data-[state=on]:bg-black hover:text-neutral-400 ring-0"
+                    >
+                        <LuCode2 className="h-5 w-5"/>
+                    </Toggle>
+
+                    <Toggle
+                        aria-label="Toggle link"
+                        className="flex hover:bg-gray-100 data-[state=on]:bg-gray-100 dark:hover:bg-neutral-700 dark:data-[state=on]:bg-black hover:text-neutral-400 ring-0"
+                    >
+                        <LuLink className="h-5 w-5"/>
+                    </Toggle>
+
+                    <Toggle
+                        aria-label="Toggle palette"
+                        className="flex hover:bg-gray-100 data-[state=on]:bg-gray-100 dark:hover:bg-neutral-700 dark:data-[state=on]:bg-black hover:text-neutral-400 ring-0"
+                    >
+                        <LuPalette className="h-5 w-5"/>
+                    </Toggle>
+
+                    <Toggle
+                        aria-label="Toggle listOrdered"
+                        className="flex hover:bg-gray-100 data-[state=on]:bg-gray-100 dark:hover:bg-neutral-700 dark:data-[state=on]:bg-black hover:text-neutral-400 ring-0"
+                    >
+                        <FaListOl className="h-5 w-5"/>
+                    </Toggle>
+
+                    <Toggle
+                        aria-label="Toggle list"
+                        className="flex hover:bg-gray-100 data-[state=on]:bg-gray-100 dark:hover:bg-neutral-700 dark:data-[state=on]:bg-black hover:text-neutral-400 ring-0"
+                    >
+                        <FaListUl className="h-5 w-5"/>
+                    </Toggle>
+
+                    <Toggle
+                        aria-label="Toggle heading1"
+                        className="flex hover:bg-gray-100 data-[state=on]:bg-gray-100 dark:hover:bg-neutral-700 dark:data-[state=on]:bg-black hover:text-neutral-400 ring-0"
+                    >
+                        <LuHeading1 className="h-5 w-5"/>
+                    </Toggle>
+
+                    <Toggle
+                        aria-label="Toggle heading2"
+                        className="flex hover:bg-gray-100 data-[state=on]:bg-gray-100 dark:hover:bg-neutral-700 dark:data-[state=on]:bg-black hover:text-neutral-400 ring-0"
+                    >
+                        <LuHeading2 className="h-5 w-5"/>
+                    </Toggle>
+
+                    <Toggle
+                        aria-label="Toggle heading3"
+                        className="flex hover:bg-gray-100 data-[state=on]:bg-gray-100 dark:hover:bg-neutral-700 dark:data-[state=on]:bg-black hover:text-neutral-400 ring-0"
+                    >
+                        <LuHeading3 className="h-5 w-5"/>
+                    </Toggle>
+                </div>
+            </div>
+
+            <div ref={editorRef} style={{height: height, width: width}} className="monaco_css rounded-lg"/>
+        </>
+    )
+}
+
+export default QuestionCommentMonacoEditor;
