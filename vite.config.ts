d02c1a07e61cd21f4cc93037cf57ea0782abc882
audgks5551 <audgsk5551@daum.net>
2024-03-28 14:40:52 +0900

    Feat: 첫 로딩 최적화

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,28 @@
 import path from "path"
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import monacoEditorPluginModule from 'vite-plugin-monaco-editor'
+
+const isObjectWithDefaultFunction = (module: unknown): module is { default: typeof monacoEditorPluginModule } => (
+    module != null &&
+    typeof module === 'object' &&
+    'default' in module &&
+    typeof module.default === 'function'
+)
+
+const monacoEditorPlugin = isObjectWithDefaultFunction(monacoEditorPluginModule)
+    ? monacoEditorPluginModule.default
+    : monacoEditorPluginModule
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    plugins: [
+        react(),
+        monacoEditorPlugin({})
+    ],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
     },
-  },
 })
