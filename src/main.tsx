511cbfddddc67cab8debd08d36d666c07f215db5
audgks5551 <audgsk5551@daum.net>
2024-03-28 16:08:59 +0900

    Feat: 첫 로딩 최적화3

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,6 @@ import 'react-toastify/dist/ReactToastify.css';
 import {Toaster} from './components/ui/toaster.tsx';
 import {ThemeProvider} from "@/context/ThemeContext.tsx";
 import React, {Suspense} from "react";
-import App from "@/App.tsx";
 
 const queryClient = new QueryClient()
 
@@ -18,6 +17,8 @@ function preloadComponent(componentLoader: () => void): void {
 }
 
 // Lazy-loaded components
+const App = React.lazy(() => import('@/App.tsx'));
+preloadComponent(() => import('@/App.tsx'));
 const Main = React.lazy(() => import('./pages/Main.tsx'));
 preloadComponent(() => import('./pages/Main.tsx'));
 const Post = React.lazy(() => import('@/pages/posts/Post.tsx'));
