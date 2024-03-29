971252dd81d44647b7b46bc564060d2c4cec8b58
audgks5551 <audgsk5551@daum.net>
2024-03-29 23:05:10 +0900

    Fix: 메모 편집에서 사이드바에 보안 메모리스트 추가

---
+++ src/components/memos/sidebar/SecurityMemos.tsx
@@ -0,0 +1,39 @@
+import {useContext} from "react";
+import {MemoContext} from "@/context/MemoContext.tsx";
+import MemoSummary from "@/components/memos/sidebar/MemoSummary.tsx";
+
+const SecurityMemos = ({isTab}: {isTab: string}) => {
+
+    const {
+        onMemoCreateSubmit,
+        findAllMemo,
+    } = useContext(MemoContext);
+
+    return (
+        <div className={`flex-1 flex flex-col overflow-y-scroll ${isTab === "tab3" ? "" : `hidden`}`}>
+            <div className="flex flex-col bg-gray-100 dark:bg-black dark:bg-opacity-40 space-y-4 flex-1 p-1">
+                <div
+                    className={`flex flex-col space-y-1`}
+                    id="tab3">
+
+                    {findAllMemo.data?.data?.length !== 0 ?
+                        findAllMemo.data?.data
+                            ?.filter(memo => memo.security === true)
+                            ?.map((memo) => {
+                            return <MemoSummary key={memo.id} memo={memo}/>
+                        })
+                        :
+                        <button
+                            onClick={onMemoCreateSubmit}
+                            className="mx-2 my-1 hover:bg-gray-200 pl-1 py-2 rounded"
+                        >
+                            새 메모 시작하기
+                        </button>
+                    }
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default SecurityMemos
