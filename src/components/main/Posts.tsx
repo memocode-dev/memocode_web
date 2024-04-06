96a341f7210ef6e763df8553a1b6964592bc6348
miruy <dbflarla4966@naver.com>
2024-04-06 17:42:53 +0900

    [enhancement] 메인 포스트 썸네일 변경

--- src/components/main/Posts.tsx
+++ src/components/main/Posts.tsx
@@ -7,10 +7,17 @@ import {MdExpandMore} from "react-icons/md";
 import timeSince from "@/components/utils/timeSince.tsx";
 import DOMPurify from "dompurify";
 import Avatar from "react-avatar";
+import {useContext} from "react";
+import {ThemeContext} from "@/context/ThemeContext.tsx";
 
 const Posts = () => {
 
     const navigate = useNavigate();
+    const {theme} = useContext(ThemeContext)
+
+    const thumbnail = "https://images.squarespace-cdn.com/content/v1/5c77350965a707ed1710a1bc/1592330659753-70M66LGEPXFTQ8S716MX/Generative+Art+by+Mark+Stock+-+Gyre+35700.jpg"
+    // const thumbnail = ""
+
 
     const {
         data: posts,
@@ -35,59 +42,117 @@ const Posts = () => {
                     item?.content?.map(post => {
                         return (
                             <div key={post.id}
-                                 className="flex flex-col scale-100 rounded-lg bg-neutral-100 dark:bg-neutral-800 hover:cursor-pointer transform transition duration-300 hover:-translate-y-2 hover:shadow-lg"
+                                 className="flex flex-col h-[310px] scale-100 rounded-lg bg-neutral-100 dark:bg-neutral-800 hover:cursor-pointer transform transition duration-300 hover:-translate-y-2 hover:shadow-lg"
                                  onClick={() => {
                                      navigate(`/@${post?.author?.username}/${post.id}`)
                                  }}
                             >
-                                <img
-                                    src="https://images.squarespace-cdn.com/content/v1/5c77350965a707ed1710a1bc/1592330659753-70M66LGEPXFTQ8S716MX/Generative+Art+by+Mark+Stock+-+Gyre+35700.jpg"
-                                    className="rounded-lg w-auto h-[50%]" alt="thumbNail"/>
+                                {thumbnail &&
+                                    <>
+                                        <img
+                                            src={thumbnail}
+                                            className="rounded-lg w-auto h-[50%]" alt="thumbNail"/>
 
-                                <div className="flex-1 flex flex-col justify-between py-2 px-3">
-                                    <div>
-                                        <div
-                                            className="text-lg font-semibold tracking-tight line-clamp-1">{post.title}</div>
-                                        <div className="markdown-body tracking-wide line-clamp-2"
-                                             style={{fontSize: 14, marginTop: 4}}
-                                             dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(post.content || "")}}></div>
-                                    </div>
-
-                                    <div className="mt-3">
-                                        <div className="flex items-center text-xs">
-                                            <div className="flex items-center space-x-1.5">
-                                                <Avatar
-                                                    name={post.author?.username}
-                                                    size="25"
-                                                    round="5px"/>
-
-                                                <div>{post.author?.username}</div>
-
+                                        <div className="flex-1 flex flex-col justify-between py-2 px-3">
+                                            <div className="h-[70px]">
                                                 <div
-                                                    className="text-gray-500 dark:text-gray-400">{timeSince(new Date(post.createdAt!))}</div>
+                                                    className="text-lg font-semibold tracking-tight line-clamp-1">{post.title}</div>
+                                                <div className="markdown-body tracking-wide line-clamp-2"
+                                                     style={{fontSize: 14, marginTop: 4}}
+                                                     dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(post.content || "")}}></div>
+                                            </div>
+
+                                            <div className="mt-3">
+                                                <div className="flex items-center text-xs">
+                                                    <div className="flex items-center space-x-1.5">
+                                                        <Avatar
+                                                            name={post.author?.username}
+                                                            size="25"
+                                                            round="5px"/>
+
+                                                        <div>{post.author?.username}</div>
+
+                                                        <div
+                                                            className="text-gray-500 dark:text-gray-400">{timeSince(new Date(post.createdAt!))}</div>
+                                                    </div>
+                                                </div>
+
+                                                <div className="flex justify-between text-xs mt-1">
+                                                    <div className="flex space-x-2">
+                                                        <div className="flex items-center space-x-1">
+                                                            <AiFillLike className="w-4 h-4"/>
+                                                            <div>0</div>
+                                                        </div>
+
+                                                        <div className="flex items-center space-x-1">
+                                                            <IoGlasses className="w-6 h-6"/>
+                                                            <div>0</div>
+                                                        </div>
+                                                    </div>
+
+                                                    <div className="flex items-center space-x-1">
+                                                        <AiOutlineComment className="w-5 h-5"/>
+                                                        <div>0</div>
+                                                    </div>
+                                                </div>
                                             </div>
                                         </div>
+                                    </>
+                                }
 
-                                        <div className="flex justify-between text-xs mt-1">
-                                            <div className="flex space-x-2">
-                                                <div className="flex items-center space-x-1">
-                                                    <AiFillLike className="w-4 h-4"/>
-                                                    <div>0</div>
-                                                </div>
+                                {!thumbnail &&
+                                    <div className="flex-1 flex flex-col justify-between py-2 px-3">
+                                        <div className="">
+                                            <div
+                                                className="text-lg font-semibold tracking-tight line-clamp-2">{post.title}</div>
+                                            <div
+                                                className="text-md mt-2 tracking-tight line-clamp-2 text-gray-600 dark:text-gray-400">summary
+                                            </div>
+                                            <div className="markdown-body tracking-wide line-clamp-6"
+                                                 style={{
+                                                     fontSize: 14,
+                                                     marginTop: 4,
+                                                     color: `${theme === "dark" ? "#9ca3af" : "#6b7280"}`
+                                                 }}
+                                                 dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(post.content || "")}}></div>
+                                        </div>
 
-                                                <div className="flex items-center space-x-1">
-                                                    <IoGlasses className="w-6 h-6"/>
-                                                    <div>0</div>
+                                        <div className="mt-3">
+                                            <div className="flex items-center text-xs">
+                                                <div className="flex items-center space-x-1.5">
+                                                    <Avatar
+                                                        name={post.author?.username}
+                                                        size="25"
+                                                        round="5px"/>
+
+                                                    <div>{post.author?.username}</div>
+
+                                                    <div
+                                                        className="text-gray-500 dark:text-gray-400">{timeSince(new Date(post.createdAt!))}</div>
                                                 </div>
                                             </div>
 
-                                            <div className="flex items-center space-x-1">
-                                                <AiOutlineComment className="w-5 h-5"/>
-                                                <div>0</div>
+                                            <div className="flex justify-between text-xs mt-1">
+                                                <div className="flex space-x-2">
+                                                    <div className="flex items-center space-x-1">
+                                                        <AiFillLike className="w-4 h-4"/>
+                                                        <div>0</div>
+                                                    </div>
+
+                                                    <div className="flex items-center space-x-1">
+                                                        <IoGlasses className="w-6 h-6"/>
+                                                        <div>0</div>
+                                                    </div>
+                                                </div>
+
+                                                <div className="flex items-center space-x-1">
+                                                    <AiOutlineComment className="w-5 h-5"/>
+                                                    <div>0</div>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
-                                </div>
+                                }
                             </div>
                         )
                     })
