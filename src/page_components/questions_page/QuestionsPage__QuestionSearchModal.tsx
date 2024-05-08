d9d324e836af9e2ec440d91ffae7d358cf40f54b
miruy <94223746+miruy@users.noreply.github.com>
2024-05-08 14:26:37 +0900

    [enhancement] Q&A 검색 기능 구현 (#118)

--- src/page_components/questions_page/QuestionsPage__QuestionSearchModal.tsx
+++ src/page_components/questions_page/QuestionsPage__QuestionSearchModal.tsx
@@ -5,20 +5,27 @@ import {FiDelete} from "react-icons/fi";
 import {Button} from "@/components/ui/button.tsx";
 import {X} from "lucide-react";
 import DOMPurify from "dompurify";
-import {faker} from "@faker-js/faker";
 import {FaRegClipboard} from "react-icons/fa";
+import {useSearchQuestion} from "@/openapi/api/questions/questions.ts";
+import {Badge} from "@/components/ui/badge.tsx";
+import {useNavigate} from "react-router-dom";
 
 const QuestionsPage__QuestionSearchModal = () => {
 
     const {modalState, closeModal} = useContext(ModalContext)
-    const [keyword, setKeyword] = useState("")
+    const naviate = useNavigate()
+    const [keyword, setKeyword] = useState<string>()
 
-    // 가짜 데이터 생성
-    const fakeDatas = Array.from({length: 10}, () => ({
-        title: faker.lorem.sentence(),
-        content: faker.lorem.paragraph(),
-        username: faker.internet.userName()
-    }));
+    const searchQuestions =
+        useSearchQuestion({
+            keyword: keyword,
+            page: 0,
+            pageSize: 20,
+        }, {
+            query: {
+                queryKey: ["QuestionsPage__QuestionSearchModal", keyword]
+            }
+        })
 
     return (
         <>
@@ -56,23 +63,23 @@ const QuestionsPage__QuestionSearchModal = () => {
 
                     {keyword && <span className="text-xs px-4 text-gray-500">검색 결과</span>}
 
-                    {fakeDatas.length === 0 &&
+                    {searchQuestions.data?.totalCount === 0 &&
                         <div className="text-center text-xs py-2">
                             <span>찾는 메모가 없습니다.</span>
                         </div>
                     }
 
                     {/* 검색 내용 */}
-                    {keyword && fakeDatas &&
+                    {keyword && searchQuestions &&
                         <div className="flex flex-col space-y-2 w-full flex-1 bg-transparent p-4 rounded">
-                            {fakeDatas.map((fakeData, index) => {
+                            {searchQuestions.data?.content?.map((searchQuestion, index) => {
                                 return (
                                     <div
                                         key={index}
                                         onClick={() => {
                                             setKeyword("")
                                             closeModal({name: ModalTypes.QUESTION_SEARCH})
-                                            // navigate(`/w/${hit._formatted.id}`)
+                                            naviate(`/questions/${searchQuestion.id}`)
                                         }}
                                         className="flex p-2 space-x-2 border border-gray-200 hover:bg-gray-100 dark:hover:bg-neutral-700 dark:border-neutral-700 rounded cursor-pointer">
 
@@ -82,15 +89,26 @@ const QuestionsPage__QuestionSearchModal = () => {
                                             <div
                                                 className="markdown-body tracking-wide line-clamp-1"
                                                 style={{fontWeight: 700}}
-                                                dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(fakeData.title || "")}}></div>
+                                                dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(searchQuestion.title || "")}}></div>
 
                                             <div className="markdown-body tracking-wide line-clamp-2"
                                                  style={{fontWeight: 500, color: "#9ca3af", fontSize: 15}}
-                                                 dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(fakeData.content || "")}}></div>
+                                                 dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(searchQuestion.content || "")}}></div>
+
+                                            <div className="flex flex-wrap">
+                                                {searchQuestion && searchQuestion.tags?.map((tag) => {
+                                                    return (
+                                                        <>
+                                                            <Badge
+                                                                className="text-xs text-white bg-indigo-300 hover:bg-indigo-400 dark:bg-indigo-500 dark:hover:bg-indigo-600 mr-1 my-1">{tag}</Badge>
+                                                        </>
+                                                    );
+                                                })}
+                                            </div>
 
                                             <div className="markdown-body tracking-wide"
                                                  style={{color: "#9ca3af", fontSize: 12}}
-                                                 dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(fakeData.username || "")}}></div>
+                                                 dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(searchQuestion?.user?.username || "")}}></div>
                                         </div>
                                     </div>
                                 )
