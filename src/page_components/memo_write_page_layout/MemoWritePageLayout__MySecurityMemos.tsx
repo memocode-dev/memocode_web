d4ff7ce16b910ef10d3ea06f57024b8f70094566
miruy <dbflarla4966@naver.com>
2024-06-17 22:39:20 +0900

    메모 편집페이지 - 내블로그/카테고리관리/시리즈관리 탭 주석처리 && 전체 탭 없애고 공개/비공개 탭으로 변경

--- src/page_components/memo_write_page_layout/MemoWritePageLayout__MySecurityMemos.tsx
+++ src/page_components/memo_write_page_layout/MemoWritePageLayout__MySecurityMemos.tsx
@@ -10,16 +10,15 @@ interface SecurityListProps {
 const MemoWritePageLayout__MySecurityMemos = ({isTab}: SecurityListProps) => {
 
     const {findAllMyMemo} = useContext(MemoContext);
-
     const securityMemos = findAllMyMemo?.data?.filter(memo => memo.security);
 
     return (
-        <div className={`flex-1 flex flex-col overflow-y-scroll ${isTab === "tab3" ? "" : `hidden`}`}>
+        <div className={`flex-1 flex flex-col overflow-y-scroll ${isTab === "tab4" ? "" : `hidden`}`}>
             <div className="flex flex-col bg-gray-100 dark:bg-black dark:bg-opacity-40 space-y-4 flex-1 p-1">
 
                 <div
                     className={`flex flex-col space-y-1`}
-                    id="tab3">
+                    id="tab4">
                     {securityMemos?.map((securityMemo) => {
                         return <MemoWritePageLayout__MemoSummaryModal key={securityMemo.id} memo={securityMemo}/>
                     })}
--- src/page_components/memo_write_page_layout/memo_write_page_layout__buttons/MemoWritePageLayout__MemoTabButtons.tsx
+++ src/page_components/memo_write_page_layout/memo_write_page_layout__buttons/MemoWritePageLayout__MemoTabButtons.tsx
@@ -7,8 +7,7 @@ const MemoWritePageLayout__MemoTabButtons = ({setIsTab, isTab}: MemoWritePageLay
 
     return (
         <div className="flex w-full my-1">
-
-            {/* tab1 - 전체 버튼 */}
+            {/* tab1 - 공개 버튼 */}
             <label
                 htmlFor="tab1"
                 className={`flex-1 flex rounded-sm py-1 px-2 select-none ${isTab === "tab1" ? `bg-primary text-primary-foreground` : `text-gray-400 hover:text-gray-800 hover:bg-gray-100 dark:hover:bg-black dark:hover:text-white`}`}>
@@ -18,26 +17,26 @@ const MemoWritePageLayout__MemoTabButtons = ({setIsTab, isTab}: MemoWritePageLay
                     }}
                     className="flex-1 flex justify-center text-sm tracking-wider"
                 >
-                    전체
+                    공개
                 </button>
             </label>
 
-            {/* tab2 - 즐겨찾기 버튼 */}
-            <label htmlFor="tab2"
-                   className={`flex-1 flex rounded-sm py-1 px-2 select-none ${isTab === "tab2" ? `bg-primary text-primary-foreground` : `text-gray-400 hover:text-gray-800 hover:bg-gray-100 dark:hover:bg-black dark:hover:text-white`}`}>
+            {/* tab2 - 비공개 버튼 */}
+            <label
+                htmlFor="tab2"
+                className={`flex-1 flex rounded-sm py-1 px-2 select-none ${isTab === "tab2" ? `bg-primary text-primary-foreground` : `text-gray-400 hover:text-gray-800 hover:bg-gray-100 dark:hover:bg-black dark:hover:text-white`}`}>
                 <button
                     onClick={() => {
-                        setIsTab("tab2");
-
+                        setIsTab("tab2")
                     }}
                     className="flex-1 flex justify-center text-sm tracking-wider"
                 >
-                    즐겨찾기
+                    비공개
                 </button>
             </label>
 
-            {/* tab3 - 보안 버튼 */}
-            <label htmlFor="tab2"
+            {/* tab3 - 즐겨찾기 버튼 */}
+            <label htmlFor="tab3"
                    className={`flex-1 flex rounded-sm py-1 px-2 select-none ${isTab === "tab3" ? `bg-primary text-primary-foreground` : `text-gray-400 hover:text-gray-800 hover:bg-gray-100 dark:hover:bg-black dark:hover:text-white`}`}>
                 <button
                     onClick={() => {
@@ -46,10 +45,23 @@ const MemoWritePageLayout__MemoTabButtons = ({setIsTab, isTab}: MemoWritePageLay
                     }}
                     className="flex-1 flex justify-center text-sm tracking-wider"
                 >
-                    보안
+                    즐겨찾기
                 </button>
             </label>
 
+            {/* tab4 - 보안 버튼 */}
+            <label htmlFor="tab4"
+                   className={`flex-1 flex rounded-sm py-1 px-2 select-none ${isTab === "tab4" ? `bg-primary text-primary-foreground` : `text-gray-400 hover:text-gray-800 hover:bg-gray-100 dark:hover:bg-black dark:hover:text-white`}`}>
+                <button
+                    onClick={() => {
+                        setIsTab("tab4");
+
+                    }}
+                    className="flex-1 flex justify-center text-sm tracking-wider"
+                >
+                    보안
+                </button>
+            </label>
         </div>
     )
 }
