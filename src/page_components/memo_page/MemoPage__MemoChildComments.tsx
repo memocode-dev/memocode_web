24768f7a00370e9b6f6a0c89f2f64341d50bd029
miruy <dbflarla4966@naver.com>
2024-07-12 03:01:03 +0900

    Fix

--- src/page_components/memo_page/MemoPage__MemoChildComments.tsx
+++ src/page_components/memo_page/MemoPage__MemoChildComments.tsx
@@ -211,8 +211,8 @@ const MemoPage__MemoChildComments = ({
             {!isLogined && !comment.deleted && comment.childMemoComments?.length === 0 &&
                 <div className="py-14 flex flex-col items-center text-gray-400">
                     <div className="flex items-center space-x-1">
-                        <BsEmojiSurprise className="w-6 h-6"/>
                         <div>답글을 남기시려면 로그인 후 이용해주세요!</div>
+                        <BsEmojiSurprise className="w-6 h-6"/>
                     </div>
                 </div>
             }
