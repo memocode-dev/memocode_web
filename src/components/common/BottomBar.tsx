02abb4125e82214b0be2cf1e313fcb7e9216a396
miruy <dbflarla4966@naver.com>
2024-07-19 18:24:59 +0900

    update

--- src/components/common/BottomBar.tsx
+++ src/components/common/BottomBar.tsx
@@ -7,15 +7,15 @@ import UpToDownButton from "@/components/ui/UpToDownButton";
 const BottomBar = () => {
 
     const goBack = () => {
-        window.history.back();
+        typeof window !== 'undefined' && window.history.back();
     };
 
     const goForward = () => {
-        window.history.forward();
+        typeof window !== 'undefined' && window.history.forward();
     };
 
     const reload = () => {
-        window.location.reload();
+        typeof window !== 'undefined' && window.location.reload();
     };
 
     return (
@@ -23,15 +23,18 @@ const BottomBar = () => {
             className="flex flex-1 md:hidden fixed bottom-0 left-0 right-0 p-1 border-t bg-background/10 backdrop-blur z-[10] h-14">
             <div className="flex flex-1 justify-around items-center">
                 <Button variant="ghost"
-                        className="w-fit h-fit px-2 hover:bg-neutral-200/50 dark:hover:bg-neutral-600/50" onClick={goBack}>
+                        className="w-fit h-fit px-2 hover:bg-neutral-200/50 dark:hover:bg-neutral-600/50"
+                        onClick={goBack}>
                     <LuArrowLeft className="w-5 h-5"/>
                 </Button>
-                <Button variant="ghost" className="w-fit h-fit px-2 hover:bg-neutral-200/50 dark:hover:bg-neutral-600/50"
+                <Button variant="ghost"
+                        className="w-fit h-fit px-2 hover:bg-neutral-200/50 dark:hover:bg-neutral-600/50"
                         onClick={reload}>
                     <LuRotateCw className="w-5 h-5"/>
                 </Button>
                 <Button variant="ghost"
-                        className="w-fit h-fit px-2 hover:bg-neutral-200/50 dark:hover:bg-neutral-600/50" onClick={goForward}>
+                        className="w-fit h-fit px-2 hover:bg-neutral-200/50 dark:hover:bg-neutral-600/50"
+                        onClick={goForward}>
                     <LuArrowRight className="w-5 h-5"/>
                 </Button>
 
