f3e47295a7b1f964bceecb460906c1a143ffd492
miruy <dbflarla4966@naver.com>
2024-07-25 23:01:45 +0900

    update : 내블로그 UI 1차 구현

--- src/app/[@username]/page.tsx
+++ src/app/[@username]/page.tsx
@@ -1,5 +1,9 @@
-export default function MyBlog(){
-    return(
-        <div className="mt-32">MyBlog</div>
+import MyBlogPage from "@/components/pages/myBlog/MyBlogPage";
+
+export default function MyBlog() {
+    return (
+        <div className="flex-1 flex flex-col py-20 mx-3 sm:mx-[50px] lg:mx-[150px] xl:mx-[220px] 2xl:mx-[350px]">
+            <MyBlogPage/>
+        </div>
     )
 }
