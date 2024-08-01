2dc13ab7956353e2c8d55139f108bb38782222f7
miruy <dbflarla4966@naver.com>
2024-08-01 15:15:58 +0900

    update : 메모 공개 후 홈에 바로 반영안되는 에러 해결

--- src/components/page_components/Layout.tsx
+++ src/components/page_components/Layout.tsx
@@ -1,31 +1,31 @@
 'use client';
 
-import {useKeycloak} from "@/context/KeycloakContext";
+import { useKeycloak } from "@/context/KeycloakContext";
 import TopBar from "@/components/common/TopBar";
 import BottomBar from "@/components/common/BottomBar";
-import {usePathname} from "next/navigation";
-import {useEffect} from "react";
+import { usePathname } from "next/navigation";
+import { useEffect } from "react";
 import RequiredLoginPage from "@/components/pages/error/RequiredLoginPage";
 
-const Layout = ({children}: { children: React.ReactNode }) => {
+const Layout = ({ children }: { children: React.ReactNode }) => {
 
     const pathname = usePathname();
     const isWRoute = pathname?.startsWith('/w') || false; // boolean으로 타입 변환
-    const {isLogined} = useKeycloak();
+    const { isLogined } = useKeycloak();
 
     useEffect(() => {
         window.scrollTo(0, 0);
     }, [pathname]);
 
     if (!isLogined && isWRoute) {
-        return <RequiredLoginPage/>;
+        return <RequiredLoginPage />;
     }
 
     return (
         <>
-            {!isWRoute && <TopBar/>}
+            {!isWRoute && <TopBar />}
             {children}
-            {!isWRoute && <BottomBar/>}
+            {!isWRoute && <BottomBar />}
         </>
     );
 }
--- src/provider/ClientProviders.tsx
+++ src/provider/ClientProviders.tsx
@@ -1,12 +1,12 @@
 'use client';
 
-import {ReactNode, Suspense} from 'react';
-import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
-import {ToastContainer} from 'react-toastify';
+import { ReactNode, Suspense } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import {KeycloakProvider} from '@/context/KeycloakContext';
-import {ModalProvider} from '@/context/ModalContext';
-import {ThemeProvider} from '@/context/ThemeContext';
+import { KeycloakProvider } from '@/context/KeycloakContext';
+import { ModalProvider } from '@/context/ModalContext';
+import { ThemeProvider } from '@/context/ThemeContext';
 import LoadingPage from "@/components/pages/loading/LoadingPage";
 
 interface ClientProvidersProps {
@@ -15,14 +15,14 @@ interface ClientProvidersProps {
 
 const queryClient = new QueryClient();
 
-const ClientProviders = ({children}: ClientProvidersProps) => {
+const ClientProviders = ({ children }: ClientProvidersProps) => {
     return (
         <QueryClientProvider client={queryClient}>
             <ThemeProvider>
-                <ToastContainer/>
+                <ToastContainer />
                 <KeycloakProvider>
                     <ModalProvider>
-                        <Suspense fallback={<LoadingPage/>}>
+                        <Suspense fallback={<LoadingPage />}>
                             {children}
                         </Suspense>
                     </ModalProvider>
