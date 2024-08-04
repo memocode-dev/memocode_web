b1d8b2ed5aae809374a4df4bffdbe212aecc4640
miruy <dbflarla4966@naver.com>
2024-08-04 18:07:40 +0900

    seo 최적화를 위한 nextjs-sitemap 적용

---
+++ next-sitemap.config.js
@@ -0,0 +1,91 @@
+import fetch from 'node-fetch';
+
+async function getMemos({page, pageSize}) {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_MEMOCODE_SERVER_URL}/memos?page=${page}&pageSize=${pageSize}`);
+    if (!res.ok) {
+        return {content: []};
+    }
+    return res.json();
+}
+
+async function getQuestions({page, pageSize}) {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_MEMOCODE_SERVER_URL}/questions?page=${page}&pageSize=${pageSize}`);
+    if (!res.ok) {
+        return { content: [] };
+    }
+    return res.json();
+}
+
+export default {
+    siteUrl: 'https://memocode.dev',
+    generateRobotsTxt: true,
+    sitemapSize: 50000,
+    changefreq: 'daily',
+    priority: 0.7,
+    exclude: [
+        '/w',
+        '/api',
+    ],
+    robotsTxtOptions: {
+        policies: [
+            { userAgent: '*', allow: '/' },
+            { userAgent: '*', disallow: '/w' },
+            { userAgent: '*', disallow: '/api' },
+        ],
+    },
+    additionalPaths: async (config) => {
+        const memos = [];
+
+        let memoPage = 0;
+        const memoPageSize = 100;
+        while (true) {
+            const searchMemos = await getMemos({page: memoPage, pageSize: memoPageSize});
+            memos.push(...searchMemos.content);
+
+            if (searchMemos.last) {
+                break;
+            }
+
+            if (memos.length > 50000) {
+                break;
+            }
+
+            memoPage = memoPage + 1;
+        }
+
+        const questions = [];
+
+        let questionPage = 0;
+        const questionPageSize = 100;
+        while (true) {
+            const searchQuestions = await getQuestions({page: questionPage, pageSize: questionPageSize});
+            questions.push(...searchQuestions.content);
+
+            if (searchQuestions.last) {
+                break;
+            }
+
+            if (questions.length > 50000) {
+                break;
+            }
+
+            questionPage = questionPage + 1;
+        }
+
+        const memoPaths = memos.map((memo) => ({
+            loc: `https://memocode.dev/@${memo.user?.username}/memos/${memo.id}`,
+            lastmod: new Date(memo.updatedAt).toISOString(),
+            changefreq: 'daily',
+            priority: 1,
+        }));
+
+        const questionPaths = questions.map((question) => ({
+            loc: `https://memocode.dev/questions/${question.id}`,
+            lastmod: new Date(question.updatedAt).toISOString(),
+            changefreq: 'daily',
+            priority: 1,
+        }));
+
+        return [...memoPaths, ...questionPaths];
+    },
+};
